7c4c591d62a7102df562abb72faf4902edcd907b
HernanHernandez <67992122+HernanGHernandez@users.noreply.github.com>
2023-01-16 15:32:32 -0800

    Added functionality to closing, opening navbar

--- roots/main.js
+++ roots/main.js
@@ -9,6 +9,7 @@
 
 let toggleBtn = document.getElementById('nav-button');
 let navBar = document.getElementById('primary-navigation')
+let closeBtn = document.querySelector('.fa-times');
 
 if (toggleBtn) {
     toggleBtn.addEventListener('click', () => {
@@ -16,7 +17,28 @@ if (toggleBtn) {
             ? toggleBtn.setAttribute('aria-expanded', false)
             : toggleBtn.setAttribute('aria-expanded', true);
         navBar.toggleAttribute('data-visible');
+        document.body.classList.toggle("no-scroll")
     });
 } else {
     console.log("Element not found in the DOM.")
 }
+
+
+if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+        navBar.hasAttribute('data-visible')
+            ? toggleBtn.setAttribute('aria-expanded', false)
+            : toggleBtn.setAttribute('aria-expanded', true);
+        navBar.toggleAttribute('data-visible');
+        document.body.classList.remove("no-scroll")
+    });
+} else {
+    console.log("Element not found in the DOM.")
+}
+
+// document.addEventListener("click", function (e) {
+//     if (navBar.hasAttribute("data-visible") && e.target.closest(".primary-navigation") === null) {
+//         toggleBtn.setAttribute("aria-expanded", false);
+//         navBar.removeAttribute("data-visible");
+//     }
+// });
